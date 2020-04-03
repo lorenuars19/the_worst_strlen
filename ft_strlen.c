8d7d9c7055d52db9b9f36f93ecb27e7e40edfa81
lorenuars19 <lorenuar.s19@gmail.com>
2020-04-04 00:23:34 +0200

    I works folks

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,19 +6,27 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/03 16:15:13 by lorenuar          #+#    #+#             */
-/*   Updated: 2020/04/03 23:02:04 by lorenuar         ###   ########.fr       */
+/*   Updated: 2020/04/04 00:20:46 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include <stdlib.h>
-#include <stdio.h>
+#ifndef VERBOSE
+# define VERBOSE 0
+#endif
 
-#define N_THREADS		25
+#if VERBOSE == 1
+# include <stdio.h>
+#endif
+
+#ifndef N_THREADS
+# define N_THREADS	50
+#endif
 
 typedef struct		s_tchain
 {
-	pthread_t		*thid;
+	pthread_t		thid;
 	char			*str;
 	size_t			res;
 	struct s_tchain	*next;
@@ -27,15 +35,12 @@ typedef struct		s_tchain
 t_tchain			*new_tchain(char *str)
 {
 	t_tchain	*new;
-	pthread_t	*thread;
 
 	new = NULL;
-	thread = 0;
 	if (!(new = (t_tchain*)malloc(sizeof(t_tchain))))
 	{
 		return (NULL);
 	}
-	new->thid = thread;
 	new->str = str;
 	new->next = NULL;
 	return (new);
@@ -78,8 +83,6 @@ void				del_tchain(t_tchain *link)
 {
 	if (link)
 	{
-		free(link->thid);
-		link->thid = NULL;
 		free(link->str);
 		link->str = NULL;
 		free(link->next);
@@ -101,32 +104,31 @@ void				clear_tchain(t_tchain **chain)
 	}
 }
 
+#if VERBOSE == 1
 void					print_tchain(t_tchain *chain)
 {
 	t_tchain	*tmp;
-
+	size_t		i;
 	tmp = chain;
+	i = 0;
 	if (!tmp)
 	{
-		puts("NULL LIST");
+		puts("NULL");
 		return ;
 	}
-	printf("chain of %lu link(s)", size_tchain(tmp));
 	while (tmp)
 	{
-		printf("[>%p | thid %lu | str \"%s\" | res %lu | next> %p]", \
-		tmp, *tmp->thid, tmp->str, tmp->res, tmp->next);
-		if (tmp->next)
-		{
-			putchar(',');
-		}
+		printf("<%lu>\t[link * %p | thread id %lu | str \"%s\" | result %lu | next *> %p]\n", \
+		i++, tmp, tmp->thid, tmp->str, tmp->res, tmp->next);
+		tmp = tmp->next;
 	}
+	printf(">> Chain of %lu link(s) <<\n", size_tchain(chain));
 }
+#endif
 
 void					*thread_len(void *arg)
 {
 	t_tchain *link = (t_tchain *)arg;
-
 	size_t	len;
 
 	len = 0;
@@ -138,53 +140,66 @@ void					*thread_len(void *arg)
 	pthread_exit(NULL);
 }
 
-#define D printf("%d\t", __LINE__);
-
 size_t					ft_strlen(char *str)
 {
-	D;
 	size_t		i;
 	t_tchain	*head;
 	t_tchain	*tmp;
+	size_t		old_value;
 
-	D;
 	head = NULL;
-	D;
 	head = tmp = NULL;
-	D;
 	if (!str)
 	{
-		D;
 		return (0);
 	}
-	D;
 	i = 0;
-	D;
 	while (i < N_THREADS)
 	{
-	D;
 		append_tchain(&head, new_tchain(str));
-	D;
+#if VERBOSE == 1
+		printf("<%lu> append\r", i);
+#endif
 		i++;
 	}
-	D;
-	print_tchain(head);
-	D;
 	tmp = head;
-	D;
 	while (tmp)
 	{
-	D;
-		pthread_create(tmp->thid, NULL, thread_len, (void *)&tmp);
-	D;
+		if(pthread_create(&tmp->thid, NULL, thread_len, (void *)tmp))
+		{
+			return (0);
+		}
+#if VERBOSE == 1
+		printf("thread %lu launched\r", tmp->thid);
+#endif
 		tmp = tmp->next;
-	D;
 	}
-	D;
+	tmp = head;
+	while (tmp)
+	{
+		if(pthread_join(tmp->thid, NULL))
+		{
+			return (0);
+		}
+#if VERBOSE == 1
+		printf("thread %lu finished\r", tmp->thid);
+#endif
+		tmp = tmp->next;
+	}
+#if VERBOSE == 1
 	print_tchain(head);
-	D;
-
+#endif
+	tmp = head;
+	old_value = head->res;
+	while (tmp)
+	{
+		if (tmp->res != old_value)
+		{
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	return (old_value);
 	pthread_exit(NULL);
-	D;
-	return (0);
+
 }
